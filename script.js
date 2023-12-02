b785f62563dcc83d070d3211f018327557041017
Caio Kotan <109546124+CaioKotan@users.noreply.github.com>
2023-12-02 11:13:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var loop=true;
+function insta(){
+    window.open("https://www.instagram.com/caio_kotan/");
+}
+function twitter(){
+    window.open("https://twitter.com/caio_kotan");
+}
+function yt(){
+    window.open("https://www.youtube.com/channel/UC4XZIO9UlDtTn3SKXsSZPhQ");
+}
+function simulation(){
+    window.open("https://itch.io/embed-upload/9212388?color=000000")
+}
